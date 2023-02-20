5a39298b8f0503205f4b3e1367b207ec84181cbd
Necnion8 <necnionch@gmail.com>
2023-02-20 17:42:01 +0900

    Add: Listenerを実装し、登録する

--- src/main/java/com/gmail/necnionch/myplugin/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/gmail/necnionch/myplugin/exampleplugin/ExamplePlugin.java
@@ -1,15 +1,32 @@
 package com.gmail.necnionch.myplugin.exampleplugin;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 // プラグインのメインクラス ExamplePlugin クラス
 // ExamplePluginクラス は JavaPluginクラス を継承します
-public final class ExamplePlugin extends JavaPlugin {
+// ExamplePluginクラス は Listenerインタフェース を実装します
+public final class ExamplePlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // プラグインが有効化される時
+
+        // Bukkitサーバーのインスタンスを取得できます
+        // getServer();  // Serverオブジェクトが返ります
+
+        // サーバーインスタンスにあるプラグインマネージャを取得します
+        // getServer().getPluginManager();  // PluginManagerオブジェクトが返ります
+
+        // プラグインマネージャの registerEvents メソッドを使って、イベントリスナー(Listener)を登録します
+        // 引数1 -> 登録するリスナーのクラス
+        // 引数2 -> 登録したプラグインのインスタンス(オブジェクト)
+        //
+        // ※ この ExamplePlugin クラスはプラグインであり、
+        // 　 Listenerもこのクラスで実装されているので、引数はどちらも自身を返す this で指定する。
+        getServer().getPluginManager().registerEvents(this, this);
+
     }
 
     @Override
