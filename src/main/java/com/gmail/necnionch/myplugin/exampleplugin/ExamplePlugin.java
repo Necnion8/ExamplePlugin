b9a0d03534ed6a89a575f89cc743a06ab68c98c7
Necnion8 <necnionch@gmail.com>
2023-02-20 17:28:45 +0900

    First commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,17 @@
+package com.gmail.necnionch.myplugin.exampleplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExamplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // プラグインが有効化される時
+    }
+
+    @Override
+    public void onDisable() {
+        // プラグインが無効化される時
+    }
+
+}
